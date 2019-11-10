c8de4be9e928fc031a9825ce7df286d862662b71
Dawid Kożykowski <39924818+dkozykowski@users.noreply.github.com>
2019-11-10 10:16:08 +0100

    Add files via upload

---
+++ structures.h
@@ -0,0 +1,14 @@
+struct variable
+{
+	char label[30];
+	int firstIndex;
+	int lastIndex;
+};
+
+struct singleCommand
+{
+	char label[30]; 
+	char type[5]; 
+	char argument1[30];
+	char argument2[30]; 
+};
