96e433cee5b2c5bcdbf6d6f9d4b1a3f8c428590f
Dawid Kożykowski <39924818+dkozykowski@users.noreply.github.com>
2019-11-14 17:15:18 +0100

    Updated arrays' names

--- memory.h
+++ memory.h
@@ -8,8 +8,6 @@ int memoryStack[100000], memoryStackPrevious[100000];
 int registers[20], registersPrevious[20];
 int registersNone[20], registersNonePrevious[20];
 int toChange[30];
-int varNumberPrevious[1000];
+int varPrevious[1000];
 
 char state[10], statePrevious[10];
-
-
